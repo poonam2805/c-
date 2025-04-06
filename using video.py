94256356c089b295adf5a9ecc2e8fe7949f27bf1
poonam <155217020+poonam2805@users.noreply.github.com>
2025-04-06 11:11:24 +0530

    Add files via upload

---
+++ using video.py
@@ -0,0 +1,26 @@
+import cv2
+from ffpyplayer.player import MediaPlayer
+
+file = "sample.mp4"
+video = cv2.VideoCapture(file)
+player = MediaPlayer(file)
+
+while True:
+    ret, frame = video.read()
+    audio_frame, val = player.get_frame()
+    
+    if not ret:
+        print("End of video")
+        break
+    
+    if cv2.waitKey(1) == ord("q"):
+        break
+    
+    cv2.imshow("Video", frame)
+
+    if val != 'eof' and audio_frame is not None:
+        img, t = audio_frame
+        # audio processing code (you can add audio processing here if necessary)
+        
+video.release()
+cv2.destroyAllWindows()
